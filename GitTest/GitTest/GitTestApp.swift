22ce183920681a596e9af950bec594534d9d5f34
Ken Song <sshow224@gmail.com>
2021-04-29 17:14:26 +0800

    fist release

---
+++ GitTest/GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Sunny on 2021/4/29.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
