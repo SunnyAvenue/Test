a2795dd3b3ea122627f787992d02a139c59613a6
Ken Song <sshow224@gmail.com>
2021-04-30 11:56:28 +0800

    test again

--- GitTest/GitTest/ContentView.swift
+++ GitTest/GitTest/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world! it's Ken!")
             .padding();
         Text("ohter name").padding()
     }
