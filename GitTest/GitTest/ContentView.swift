379b761e311dfd673e688b3eefe35f04ee43594b
Ken Song <sshow224@gmail.com>
2021-05-08 11:38:43 +0800

    新练习

--- GitTest/GitTest/ContentView.swift
+++ GitTest/GitTest/ContentView.swift
@@ -7,11 +7,26 @@
 
 import SwiftUI
 
+
+
+ //[上一页]（@previous)
+
+/**
+ : [next](@next)
+ 
+ */
+ 
+ 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! it's Ken!")
-            .padding();
-        Text("ohter name").padding()
+        Text("Hello, world! it's Ken!").padding()
+        
+//        let number = 12
+        
+        let function = (12, "wrong")
+        
+        Text("test is \(function.0)")
     }
 }
 
---
+++ GitTest/GitTest/基础练习01.swift
@@ -0,0 +1,16 @@
+//
+//  基础练习01.swift
+//  GitTest
+//
+//  Created by Sunny on 2021/5/6.
+//
+
+import Foundation
+
+
+//[下一页](@next)
+
+func testApp1() {
+    let Http404Error = (404, "Not Found!");
+    print("the string is \(Http404Error)");
+}
